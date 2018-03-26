583c4586f5216f9dbc71f648719eb8f898cb1e2c
Lee <Lee@DESKTOP-1IRST1I>
2018-03-26 15:21:41 +0900

    ㅅㅅ

--- Sadan/src/main/java/com/sadan/firsttest/service/First_board_Service.java
+++ Sadan/src/main/java/com/sadan/firsttest/service/First_board_Service.java
@@ -1,11 +1,20 @@
 package com.sadan.firsttest.service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.activation.DataSource;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.transaction.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.TransactionManagementConfigurationSelector;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import com.sadan.firsttest.mapper.First_board_Mapper;
 
@@ -19,11 +28,25 @@ public class First_board_Service {
 
 
 	public Map<String, Object> List(Map<String, Object> map2) throws Exception{
+		DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+		def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+		DataSourceTransactionManager txManager = null;
+		TransactionStatus status = txManager.getTransaction(def);
 		
-		System.out.println(map2);
-		List<Map<String, Object>> board_List = 	first_board_Mapper.selectList1(map2); 
-		System.out.println(board_List);
-		return null;
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		try {
+			System.out.println(map2);
+			List<Map<String, Object>> board_List = 	first_board_Mapper.selectList1(map2); 
+			resultMap.put("board_List", board_List);
+			System.out.println(board_List);
+			txManager.commit(status);
+			
+			
+		} catch (Exception e) {
+		e.printStackTrace();
+		txManager.rollback(status);
+		}
+		return resultMap;
 	}
 
 }
